c943ae7754346a5f07e225254cbbe487485b8a9f
zzzun <wbs0810@gmail.com>
2023-08-28 16:33:08 +0900

    사이드바에 회원정보, 로그아웃 메뉴 추가, 폰트 css 추가, 회원인증 페이지 사이드바 분리

--- lms/src/main/java/com/scit/lms/controller/StudentController.java
+++ lms/src/main/java/com/scit/lms/controller/StudentController.java
@@ -4,14 +4,20 @@ import com.scit.lms.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
-@RequestMapping("teacher")
+@RequestMapping("student")
 public class StudentController {
 
     @Autowired
     StudentService service;
+
+    @GetMapping("index")
+    public String index() {
+        return "testView/index";
+    }
 
 }
