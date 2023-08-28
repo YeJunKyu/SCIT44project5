1496193cceb0f14c88e1f743b54931feb85fa280
Mozz-Aic <drgwhite97@gmail.com>
2023-08-28 17:49:15 +0900

    교사 공지 글쓰기 (구현중...)

--- lms/src/main/java/com/scit/lms/dao/TeacherDAO.java
+++ lms/src/main/java/com/scit/lms/dao/TeacherDAO.java
@@ -1,7 +1,10 @@
 package com.scit.lms.dao;
 
+import com.scit.lms.domain.Board;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TeacherDAO {
+    // 공지 글쓰기
+    void writeNotice(Board board);
 }
--- lms/src/main/java/com/scit/lms/service/TeacherService.java
+++ lms/src/main/java/com/scit/lms/service/TeacherService.java
@@ -1,4 +1,8 @@
 package com.scit.lms.service;
 
+import com.scit.lms.domain.Board;
+
 public interface TeacherService {
+    // 공지 글쓰기
+    void write(Board board);
 }
