28465b71d51789d2610f932d7cd34d813a83e590
zzzun <wbs0810@gmail.com>
2023-09-01 14:32:44 +0900

    일정 시작

--- lms/src/main/java/com/scit/lms/controller/ScheduleController.java
+++ lms/src/main/java/com/scit/lms/controller/ScheduleController.java
@@ -1,7 +1,22 @@
 package com.scit.lms.controller;
 
+import com.scit.lms.service.NoticeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
+@RequestMapping("schedule")
 public class ScheduleController {
+
+    //@Autowired
+    //NoticeService service;
+
+    @GetMapping({"","/"})
+    public String notice() {
+        return "boardView/schedule/schedule";
+    }
 }
