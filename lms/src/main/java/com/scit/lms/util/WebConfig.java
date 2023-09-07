c9464109916cf3de3ced80d53e43d84bfea71734
NAGTA0617 <leeheewon93@gmail.com>
2023-09-07 17:17:52 +0900

    변수명 수정 // 페이지오류수정

--- lms/src/main/java/com/scit/lms/util/WebConfig.java
+++ lms/src/main/java/com/scit/lms/util/WebConfig.java
@@ -7,13 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${upload.path}")
-    private String uploadPath;
+    @Value("${memberUpload.path}")
+    private String memberUploadPath;
     @Value("${memberphoto.path}")
     private String memberphotoPath;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(uploadPath)
+        registry.addResourceHandler(memberUploadPath)
                 .addResourceLocations(memberphotoPath);
     }
 }
