8a942f0425da703b2e60f340e174ea8f31fc3e1f
NAGTA0617 <leeheewon93@gmail.com>
2023-09-07 16:27:04 +0900

    회원정보폼에 사진띄우기 유틸에 WebConfig 추가 //리소스핸들러

--- lms/src/main/java/com/scit/lms/util/WebConfig.java
+++ lms/src/main/java/com/scit/lms/util/WebConfig.java
@@ -1,4 +1,16 @@
 package com.scit.lms.util;
 
-public class WebMvcConfig {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/memberphoto/**")
+                .addResourceLocations("file:///c:/lms/uploadFile/memberphoto/");
+    }
 }
